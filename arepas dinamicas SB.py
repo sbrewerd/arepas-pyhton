dfe3ebb33896c67ea60db4227e8a2e73e99a260b
sbrewerd <122575973+sbrewerd@users.noreply.github.com>
2024-09-15 13:46:13 -0400

    Add files via upload

---
+++ arepas dinamicas SB.py
@@ -0,0 +1,23 @@
+print("vamos hacer arepas!")
+agua= int(input("porfavor indique cantidad de agua en tazas"))
+print ("el agua es", agua)
+
+harina = int(input("porfavor indique cantidad de harina en tazas"))
+print ("la harina es", harina)
+
+sal = int(input("porfavor indique cantidad de sal en cdtas"))
+print ("la sal es", sal)
+
+aceite = int(input("porfavor indique cantidad de aceite en cdas"))
+
+bol= agua + harina+sal
+print ("el bol tiene", bol)
+
+budare= bol+ aceite
+print("en el budare hay", budare)
+
+budare= budare*0.9
+
+plato=budare
+print("el plato tiene", plato)
+
